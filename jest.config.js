21bcccce18e501e2bf046c3d6b3c31fc37174e5a
Marcelo Risoli <celorisoli@gmail.com>
2019-01-28 21:38:27 +0100

    update jest and circleci config

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ const TEST_REGEX = '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|js?|tsx?|ts?)$';
 module.exports = {
   collectCoverage: true,
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  reporters: ['default', 'jest-junit'],
   setupFiles: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   testRegex: TEST_REGEX,
