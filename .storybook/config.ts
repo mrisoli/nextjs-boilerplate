8351e989736f6aad39568fd2c7d2054ff5730873
Marcelo Risoli <celorisoli@gmail.com>
2019-02-13 23:18:31 +0100

    add percy

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,4 +1,8 @@
-import { configure } from '@storybook/react';
+import createPercyAddon from '@percy-io/percy-storybook';
+import { configure, getStorybook, setAddon } from '@storybook/react';
+
+const { percyAddon, serializeStories } = createPercyAddon();
+setAddon(percyAddon);
 
 // automatically import all files ending in *.stories.tsx
 const req = require.context('../stories', true, /.stories.tsx$/);
@@ -8,3 +12,4 @@ function loadStories() {
 }
 
 configure(loadStories, module);
+serializeStories(getStorybook);
