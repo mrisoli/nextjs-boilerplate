2a0c231eade223e33974308277ad2e8972ad624f
Marcelo Risoli <celorisoli@gmail.com>
2019-04-02 19:51:20 +0200

    add next-progressbar

--- next.config.js
+++ next.config.js
@@ -1,2 +1,6 @@
-const withTypescript = require('@zeit/next-typescript')
-module.exports = withTypescript()
+const withProgressBar = require('next-progressbar');
+const withTypescript = require('@zeit/next-typescript');
+let config = {};
+config = withProgressBar(config);
+config = withTypescript(config);
+module.exports = config;
